e22e7ea7ac66bf4167ae04bedf36ccb4f23f59c3
S. Zavala <szavala3@outlook.com>
2021-04-14 11:45:48 -0400

    Revise Hat and add draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -5,7 +5,10 @@ Probability Calculator by Sofia Zavala
 
 class Hat:
     def __init__(self, **contents):
-        self.contents = list(contents.keys())
+        self.contents = [key for key, value in contents.items() for x in range(value)]
+    
+    def draw(self, num):
+        pass
 
 
 if __name__ == '__main__':
