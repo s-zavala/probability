21240f70dc8d8f1a54eae23eab43068ca0a0c134
S. Zavala <szavala3@outlook.com>
2021-04-14 11:36:25 -0400

    Add Hat class and test

---
+++ prob_calculator.py
@@ -0,0 +1,13 @@
+"""
+Probability Calculator by Sofia Zavala
+"""
+
+
+class Hat:
+    def __init__(self, **contents):
+        self.contents = list(contents.keys())
+
+
+if __name__ == '__main__':
+    bowler = Hat(red=5, blue=5)
+    print('spam')
