eccb840b1b9e47aef8a7fc868a49cc9021bcc739
S. Zavala <szavala3@outlook.com>
2021-04-15 14:08:48 -0400

    Revise hat.draw and experiment, Add gitignore

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,68 +2,70 @@
 Probability Calculator by Sofia Zavala
 """
 import random
+import copy
 
 
 class Hat:
-    def __init__(self, **contents):
-        self.contents = [key for key, value in contents.items()
+    def __init__(self, **kwargs):
+        self.contents = [key for key, value in kwargs.items()
                          for x in range(value)]
-        # self.size = len(self.contents)
 
-    def draw(self, num):
+    def draw(self, num: int):
         """
         Given a number, choose at random a ball from
         self.contents without replacement.
-        Args: num=int
-        Return a list of strings=the balls picked.
+        Return a list of strings of the balls picked.
         """
-        sample = self.contents[:]
-        balls = []
-        for draw in range(num):
-            try:
-                ball = random.choice(sample)
-            except IndexError:
-                sample = self.contents[:]
-                ball = random.choice(sample)
-            sample.remove(ball)
-            balls.append(ball)
-        return balls
-        # Using random.choices()
-        # result = []
-        # if num > self.size:
-        #     reps = num // self.size
-        #     extra = num % self.size
-        #     for x in range(reps):
-        #         draw = random.choices(sample, k=self.size)
-        #         result.extend(draw)
-        #     draw = random.choices(sample, k=extra)
-        #     result.extend(draw)
-        #     return result
-        # else:
-        #     return random.choices(sample, k=num)
+        if num >= len(self.contents): return self.contents
+        rec = []
+        # Record balls picked.
+        for trial in range(num):
+            # Repeat num times.
+            ball = random.choice(self.contents)
+            # Select a random ball.
+            rec.append(ball)
+            # Store the picked ball in rec.
+            self.contents.remove(ball)
+            # Remove the picked ball from contents.
+        return rec
 
 
-def experiment(hat, expected_balls: dict, num_balls_drawn: int, num_experiments: int):
+def experiment(hat, expected_balls: dict, num_balls_drawn: int,
+               num_experiments: int):
     """
     Args:
-    hat= a Hat obj
-    expected_balls= a dict with keys= ball colors and
-     values= expected number of draws
+    hat= a Hat object
+    expected_balls= a dict with keys of ball colors and
+     values of expected number of draws
         eg. {'red': 2, 'green': 3}
     num_balls_drawn= number of balls drawn for each experiment
     num_experiements= number of trials
     Returns a probability.
     """
-    prob = 1
+    def compare(list1, list2):
+        copy = list2[:]
+        for x in list1:
+            if x in copy:
+                copy.remove(x)
+                continue
+            else:
+                return False
+        return True
+
+    event = Hat(**expected_balls)
+
+    success = 0
     for num in range(num_experiments):
-        res = hat.draw(num_balls_drawn)
-        count = dict.fromkeys()
+        cp = copy.deepcopy(hat)
+        res = cp.draw(num_balls_drawn)
+        if compare(event.contents, res):
+            success += 1
+        else: continue
+    prob = success / num_experiments
     return prob
 
 
 if __name__ == '__main__':
     bowler = Hat(red=5, blue=5)
-    a = bowler.draw(5)
-    b = bowler.draw(10)
-    c = bowler.draw(20)
-    print('spam')
+    p = experiment(bowler, {'red': 2, 'blue': 2}, 5, 100)
+    print('Probability: ', p)
