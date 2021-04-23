51d8132600008bc947789736002b90fb6afeb7b1
S. Zavala <szavala3@outlook.com>
2021-04-23 15:37:51 -0400

    Revise docstrings

--- prob_calculator.py
+++ prob_calculator.py
@@ -8,24 +8,26 @@ import copy
 
 class Hat:
     def __init__(self, **kwargs):
+        """ Define the sample space.
+
+        Keyword args:
+        keys -- types of balls
+        values -- quantity of each type
+        """
         self.contents = [key for key, value in kwargs.items()
                          for x in range(value)]
 
     def draw(self, num: int):
         """
-        Given a number, choose at random a ball from
-        self.contents without replacement.
-        Return a list of strings of the balls picked.
+        Pick a ball at random from contents num times, w/o replacement.
+        Return a list of strings of the type of balls picked.
         """
         if num >= len(self.contents): return self.contents
         rec = []
-        # Record balls picked.
         for trial in range(num):
-            # Repeat num times.
             ball = random.choice(self.contents)
             # Select a random ball.
             rec.append(ball)
-            # Store the picked ball in rec.
             self.contents.remove(ball)
             # Remove the picked ball from contents.
         return rec
@@ -34,14 +36,15 @@ class Hat:
 def experiment(hat, expected_balls: dict, num_balls_drawn: int,
                num_experiments: int):
     """
+    Simulate an experiment of drawing a combination of balls from a hat.
+
     Args:
-    hat= a Hat object
-    expected_balls= a dict with keys of ball colors and
-     values of expected number of draws
-        eg. {'red': 2, 'green': 3}
-    num_balls_drawn= number of balls drawn for each experiment
-    num_experiements= number of trials
-    Returns a probability.
+    hat -- a Hat object
+    expected_balls -- a dict with keys of ball colors and
+    values of expected number of draws eg. {'red': 2, 'blue': 3}
+    num_balls_drawn -- number of balls drawn for each experiment
+    num_experiements -- number of trials
+    Returns the probability of the drawing a certain combination.
     """
     def compare(list1, list2):
         copy = list2[:]
@@ -67,6 +70,6 @@ def experiment(hat, expected_balls: dict, num_balls_drawn: int,
 
 
 if __name__ == '__main__':
-    bowler = Hat(red=5, blue=5)
-    p = experiment(bowler, {'red': 2, 'blue': 2}, 5, 100)
+    bowler = Hat(magenta=5, cerulean=5, polkadot=10)
+    p = experiment(bowler, {'magenta': 1, 'cerulean': 1, 'polkadot': 1}, 3, 1000)
     print('Probability: ', p)
